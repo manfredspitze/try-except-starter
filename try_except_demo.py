deafa3b21fae7fb105c8b04e393d6bfd329ec1fd
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-10-17 08:05:51 -0400

    Update try_except_demo.py

--- try_except_demo.py
+++ try_except_demo.py
@@ -1,3 +1,14 @@
+# The maximum index for the list below is 4
+my_list = [75, 88, 82, 91, 97]
+
+try:
+    # Attempt to access an out of range index
+    index = 10
+    print(my_list[index])
+except IndexError:
+    print(f"Error: Index {index} is out of range for the list.")
+
+
 # while True:
 #     try:
 #         age = int(input("Enter your age: "))
@@ -7,11 +18,11 @@
 #     except ValueError as e:
 #         print(f"Invalid input: {e}")
 
-while True:
-    try:
-        age = int(input("Enter your age: "))
-        if age < 0:
-            raise ValueError("Age cannot be negative.")
-        break
-    except ValueError as e:
-        print(f"Invalid input: {e}")
+# while True:
+#     try:
+#         age = int(input("Enter your age: "))
+#         if age < 0:
+#             raise ValueError("Age cannot be negative.")
+#         break
+#     except ValueError as e:
+#         print(f"Invalid input: {e}")
